084e05591c6ed7eb7abef72e0d4a8813c2875907
= <roulit77@gmail.com>
2021-02-04 08:33:29 +0100

    step1-solution done

--- main.js
+++ main.js
@@ -2,8 +2,8 @@
 const fn = {
     factorial(num) {
         // https://fr.wikipedia.org/wiki/Factorielle
+        // To use rescursion you'll have to call module.exports.factorial
         // Your code goes here you MUST use recursion
-        // This is the base case.
         if (num === 0) return 1;
         // This is the recursive one.
         else return num * module.exports.factorial(num - 1);
