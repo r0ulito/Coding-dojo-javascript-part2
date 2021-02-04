3253b827ddf8e1932962598214d982c46f357c5b
= <roulit77@gmail.com>
2021-02-04 08:36:38 +0100

    changed test suite. minor changes in main.js

--- main.test.js
+++ main.test.js
@@ -1,9 +1,20 @@
 const fn = require ('./main');
 
-test('use recursion', () => {
-    const spy = jest.spyOn(fn, 'factorial');
-    const result = fn.factorial(5);
+describe('the function factorial', () => {
+  describe('must use recursion', () => {
+    test('use recursion', () => {
+      const spy = jest.spyOn(fn, 'factorial');
+      const result = fn.factorial(5);
+  
+      expect(spy).toHaveBeenCalledTimes(6);
+      spy.mockRestore();
+    });
+  })
 
-    expect(spy).toHaveBeenCalledTimes(6);
-    spy.mockRestore();
-  });
+  describe('must returns 120 when 5 is the parameter', () => {
+    test('returns 120', () => {
+      const result = fn.factorial(5);
+      expect(result).toEqual(120);
+    })
+  })
+})
