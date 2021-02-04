d8879a7ef7202066b4556015c1a0c867bef8cba2
= <roulit77@gmail.com>
2021-02-04 09:19:15 +0100

    step-2 solution finished

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const fn = {
         // https://fr.wikipedia.org/wiki/Suite_de_Fibonacci
         // To use rescursion you'll have to call module.exports.fibonnacci
         // Your code goes here you MUST use recursion
+        if (num <= 1) return 1;
+
+        return module.exports.fibonnacci(num - 1) + module.exports.fibonnacci(num - 2);
     }
 
 }
